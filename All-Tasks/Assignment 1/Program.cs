8706ad3ab9ba527563676f38517bdc89dc150628
Blake James Hadaway <Blake.j.hadaway@gmail.com>
2022-02-15 22:20:08 -0500

    Add project files.

---
+++ All-Tasks/Assignment 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input, result;
+
+            Console.WriteLine("Temperature Calculator");
+
+            Console.WriteLine("Please enter a Temperature in Celsius:");
+            input = Convert.ToSingle(Console.ReadLine());
+
+            result = (input * 1.8) + 32;
+
+            Console.WriteLine("\nBlake Hadaway - Assignment 1 Part 2\n");
+            Console.WriteLine("Name: Blake Hadaway");
+            Console.WriteLine("Email: Blake.j.hadaway@gmail.com");
+            Console.WriteLine("The Celsius temperature of " + input + " degrees converts to " + result + " in fahrenheit");
+            Console.WriteLine("Experience: Moderate");
+            Console.WriteLine("Country: Canada");
+
+        }
+    }
+}
